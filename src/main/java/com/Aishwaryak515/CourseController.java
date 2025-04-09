65c338cce38cf7e84dfbbe991a702b909d230c0b
AishwaryaK515 <kamurthiaishwarya515@gmail.com>
2025-04-09 09:39:51 +0530

    Update CourseController.java

--- src/main/java/com/Aishwaryak515/CourseController.java
+++ src/main/java/com/Aishwaryak515/CourseController.java
@@ -1,24 +1,31 @@
 package com.aishwaryak515;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @RestController
+@RequestMapping("/course-svc")
 public class CourseController {
+
     private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
-//create two endpoints getAllAwsServices and getAllDevopsTools
+
     @GetMapping("/getAllAwsServices")
     public List<String> getAllAwsServices(){
-        System.out.println("added for failing sonar build");
-        return Arrays.asList("ec2","iam","rds","ecr","eks");
+        logger.info("Returning list of AWS services...");
+        return Arrays.asList("EC2", "IAM", "RDS", "ECR", "EKS");
     }
 
     @GetMapping("/getAllDevopsTools")
     public List<String> getAllDevopsTools(){
-        return Arrays.asList("git","maven","sonar","nexus","jenkins");
+        logger.info("Returning list of DevOps tools...");
+        return Arrays.asList("Git", "Maven", "SonarQube", "Nexus", "Jenkins");
     }
 }
+
