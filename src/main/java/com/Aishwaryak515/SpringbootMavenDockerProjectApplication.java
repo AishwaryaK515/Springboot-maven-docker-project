5ecea0d22103eb0db7decdacd0d17295d4610ae3
root <root@ip-172-31-7-81.ap-south-1.compute.internal>
2025-04-09 03:28:15 +0000

    added src file

---
+++ src/main/java/com/Aishwaryak515/SpringbootMavenDockerProjectApplication.java
@@ -0,0 +1,14 @@
+package com.Aishwaryak515;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenDockerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMavenDockerProjectApplication.class, args);
+    }
+
+}
+
